09d510238b9fe83a7b1535f202a132b90c65e409
unknown <ramalingam@5gindia.net>
2021-11-17 18:44:21 +0530

    test case token authentication

--- road_map/settings.py
+++ road_map/settings.py
@@ -117,14 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 #     ]
 #}
 REST_FRAMEWORK = {
-'DEFAULT_AUTHENTICATION_CLASSES':(
-    'rest_framework_simplejwt.authentication.JWTAuthentication',
-
-),
-'DEFAULT_PERMISSION_CLASSES':(
-    'rest_framework.permissions.IsAuthenticated',
-)
-
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
 
 # Internationalization
